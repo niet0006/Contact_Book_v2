7492a18a27f3b52620b1165141486dccef917d8a
Carlos <niet0006@algonquinlive.com>
2023-07-31 11:21:29 -0400

    Completes Assignment

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import App from './App.vue'
 import ContactList from './views/ContactList.vue'
 import ContactDetails from './views/ContactDetails.vue'
 
-// import ContactCard from './components/ContactCard.vue'
+
 
 const router = createRouter({
   history: createWebHistory(),
