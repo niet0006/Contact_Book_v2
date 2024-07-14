4c5c275911dd7b88f5e441128fabfb1197d3205e
Carlos <niet0006@algonquinlive.com>
2024-07-14 16:43:17 -0400

    locally

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Contact_Book_v2/',  // Replace 'repository-name' with your actual repository name
+  base: '/',
   plugins: [vue()],
 })
