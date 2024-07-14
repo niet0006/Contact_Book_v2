92e0fdb6b8f7a9f2f90f2be58cafec01c531a9ff
Carlos <niet0006@algonquinlive.com>
2024-07-14 16:05:45 -0400

    Readme

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/contactbook/',  // Replace 'repository-name' with your actual repository name
   plugins: [vue()],
 })
